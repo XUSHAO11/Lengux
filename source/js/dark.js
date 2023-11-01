3af7f293c9185bb4b3861acda732b9a320da061c
ysx <1217671134@qq.com>
2023-11-01 15:46:37 +0800

    add-dark

---
+++ source/js/dark.js
@@ -0,0 +1,36 @@
+// 目标元素
+const targetElement = window.document.body
+
+// 创建 MutationObserver 实例
+const observer = new MutationObserver(function(mutationsList) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "attributes" && mutation.attributeName === "class") {
+      const currentClassNames = targetElement.classList;
+
+      if (currentClassNames.contains('darkmode--activated')) {
+        // 类名 'darkmode--activated' 已添加
+        // 在此执行你的操作
+        console.log("Dark mode activated");
+          $('.header-top').css({
+            'border-bottom': '1px solid',
+          });
+          $('.position-static').css({
+            'color': '#f4f4f4',
+          });
+          $('.list-unstyled>li>a').css({
+            'color': '#f4f4f4',
+          });
+          $('.fst-italic').css({
+            'color': '#f4f4f4',
+          });
+          
+      }
+    }
+  }
+});
+
+// 配置 MutationObserver 监听属性改变
+const config = { attributes: true };
+
+// 开始监听目标元素
+observer.observe(targetElement, config);
